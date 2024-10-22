95be5116129d3ede28cfb892101345e8e901f19f
killi1812 <cvok.fran@gmail.com>
2024-10-22 21:39:51 +0200

    First migration

--- PatientManagerServices/Models/Patient.cs
+++ PatientManagerServices/Models/Patient.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
 namespace PatientManagerServices.Models;
 
 public class Patient
 {
-    public int Id;
+    public int Id { get; set; }
     public Guid Guid { get; set; } = System.Guid.NewGuid();
     public string Name { get; set; }
     public string Surname { get; set; }
